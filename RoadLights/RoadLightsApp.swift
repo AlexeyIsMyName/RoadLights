fb457e8ea4c169a376d2a1cd8f0b78112785b6db
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-03-26 13:17:48 +0300

    Initial Commit

---
+++ RoadLights/RoadLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadLightsApp.swift
+//  RoadLights
+//
+//  Created by ALEKSEY SUSLOV on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct RoadLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
